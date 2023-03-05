1981f04c3f90ece2e5f2a5a8bebd89a390b86dbc
Ashihmin Kirill <kirill142ka@gmail.com>
2023-03-06 01:03:38 +0700

    About Me + Skills + Fix bugs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,15 @@ namespace MyPortfolio.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
